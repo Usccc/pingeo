709e6376ee5bef82982151fb8841b132dd43eaac
Usccc <papermakan@outlook.com>
2026-04-18 20:29:34 +0800

    Reduce socket timeout for Bedrock server ping
    Updated socket timeout from 300000 to 30000 milliseconds
    That's because 300s is tooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo long..

--- pingeo/src/main/java/top/pigimag/pingeo/pi/BedrockServerPinger.java
+++ pingeo/src/main/java/top/pigimag/pingeo/pi/BedrockServerPinger.java
@@ -187,7 +187,7 @@ public class BedrockServerPinger {
         };
 
         try (DatagramSocket socket = new DatagramSocket()) {
-            socket.setSoTimeout(300000);
+            socket.setSoTimeout(30000);
             long start = System.nanoTime();
 
             ByteBuffer request = ByteBuffer.allocate(1 + Long.BYTES + magic.length + Integer.BYTES)
